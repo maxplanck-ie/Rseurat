6037fc7856aca2b689bea4e8eeec1544f0ff753c
adRn-s <salatino@omics.dev>
2023-04-20 11:18:20 +0200

    added umap hints, polished comments

--- rmd/27_brushedPlot.R
+++ rmd/27_brushedPlot.R
@@ -2,25 +2,28 @@
 
 # Similar purposes can be achieved through Seurat's built-in functions:
 # https://satijalab.org/seurat/articles/visualization_vignette.html#interactive-plotting-features
-# but our approach is more general, helpful to many data wrangling scenarios.
+# but our approach is more general, and helpful to many data wrangling scenarios.
 
-# Run App.
+# Run App. Click-n-drag to select cells you'd like to filter.
 
 # Convert row.names into a vector by:
 #
 # copy and pasting the rendered table
-# at bottom into an output text file
-# and processing it with GNU/Linux...
+# at bottom into an output text file,
+# and then process it with GNU shell...
 #
 # $ cat output.txt | cut -d' ' -f1 | tr '\n' ',' | sed -e 's/,/", "/g'
 #
-# Ignore the 3 starting characters (quote, comma, and space),
-# and the 3 ending characters (comma, space, and quote),
-# select everything in the middle, paste inside a `c()`,
-# assign to a variable, and... voilá!
-# 
-# Ready to apply filter:
+# Ignore the 3 starting characters (quote, comma, and space), and the 3 ending
+# characters (comma, space, and quote); select everything in the middle: "xxx",
+# "yyy", "zzz" and, paste inside `c()`, assign to a variable, e.g. cellsToDrop,
+# and... voilá! To apply filter in R-lang:
+#
 # myData <- myData[,!colnames(myData) %in% cellsToDrop]
+
+# If you preferred to use UMAP representation, you can get the embeddings:
+# brushData <- as.data.frame(myData@reductions$umap@cell.embeddings)
+# Remember to adjust xvar and yvar accordignly (UMAP_{1,2})...
 
 options(max.print=999)
 
